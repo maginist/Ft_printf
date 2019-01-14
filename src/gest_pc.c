479a6034d07c60e56b0b4e37cc9da101133a415f
Mathieu GINISTY <maginist@e3r4p13.42.fr>
2019-01-14 17:14:31 +0100

    fix tdc %

--- src/gest_pc.c
+++ src/gest_pc.c
@@ -6,7 +6,7 @@
 /*   By: maginist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/09 17:04:46 by maginist          #+#    #+#             */
-/*   Updated: 2019/01/11 14:34:19 by maginist         ###   ########.fr       */
+/*   Updated: 2019/01/14 17:13:39 by maginist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ void	gest_pc(t_data *data, va_list ap)
 {
 	(void)ap;
 	(void)*data;
-	ft_putchar('%');
 	data->size_aff += 1;
+	data->tdc -= data->size_aff;
+	if (data->moins == 0)
+		printdc(data->tdc);
+	write(1, "%", 1);
+	if (data->moins == 1)
+		printdc(data->tdc);
 }
